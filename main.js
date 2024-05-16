c1e976def943d2f6997899969fa4db41a6dbcb8d
“[firstname <[gmugsk@gmail.com]>
2024-05-16 11:56:29 +0500

    Assignment 15

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+/*Changing Guest List: You just heard that one of your guests can’t
+make the dinner, so you need to send out a new set of invitations.
+ You’ll have to think of someone else to invite.*/
+let guestArr = ["Syed", "Dilshad", "Ahmed", "Riaz"];
+let message = ` ${guestArr[3]} can not attend on Dinner`;
+console.log(message);
+//console.log(canNotAttend + " " "can not attend the dinner")
---
+++ main.ts
@@ -0,0 +1,10 @@
+/*Changing Guest List: You just heard that one of your guests can’t 
+make the dinner, so you need to send out a new set of invitations.
+ You’ll have to think of someone else to invite.*/
+
+ let guestArr: string[] = ["Syed", "Dilshad", "Ahmed","Riaz"]
+ let message: string =` ${guestArr[3]} can not attend on Dinner`;  
+ console.log(message);
+//console.log(canNotAttend + " " "can not attend the dinner")
+
+
